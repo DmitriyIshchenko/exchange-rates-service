9a79f623e5a6a4fb0db24a7426a727fd0e0013e8
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2021-08-10 14:09:49 +0300

    Add graph reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import exchangerReducer from '../features/exchanger/exchangerSlice';
+import graphReducer from "../features/graph/graphSlice"
 
 
 export const store = configureStore({
   reducer: {
-    exchanger: exchangerReducer
+    exchanger: exchangerReducer,
+    graph: graphReducer
   }
 });
