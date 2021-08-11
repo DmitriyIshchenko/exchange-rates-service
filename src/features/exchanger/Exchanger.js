5fa8674279e9315a14bfdadf922b225c61b0110e
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2021-08-11 22:48:42 +0300

    Add a display of today's date

--- src/features/exchanger/Exchanger.js
+++ src/features/exchanger/Exchanger.js
@@ -11,7 +11,7 @@ import {
     setInput,
     convertAsync
 } from "./exchangerSlice"
-
+import moment from 'moment';
 
 
 export function Exchanger() {
@@ -33,41 +33,56 @@ export function Exchanger() {
         if (e.keyCode === 13) dispatch(convertAsync(input))
     }
     return (
-        <div>
-            <input
-                placeholder="15 usd in rub"
-                onKeyDown={pressEnter}
-                value={input}
-                onChange={(e) => dispatch(setInput(e.target.value))} />
-            <button onClick={() => dispatch(convertAsync(input))}>Click</button>
-            <br />
-            <select onChange={(e) => dispatch(setBaseAsync(e.target.value))}>
-                {Object.keys(symbols).map(item => {
-                    const text = `${item} - ${symbols[item].description}`
-                    return item === base ?
-                        <option value={item} selected="selected">{text}</option>
-                        : <option value={item}>{text}</option>
-                })}
-            </select>
-            <select onChange={(e) => dispatch(setTarget(e.target.value))}>
-                {Object.keys(symbols).map(item => {
-                    const text = `${item} - ${symbols[item].description}`
-                    return item === target ?
-                        <option value={item} selected="selected">{text}</option>
-                        : <option value={item}>{text}</option>
-                })}
-            </select>
-            <br />
-            <input
-                type="number"
-                pattern="[0-9]*"
-                value={exchangeFrom}
-                onChange={(e) => dispatch(setExchangeFrom(e.target.value))} />
-            <input
-                type="number"
-                pattern="[0-9]*"
-                value={exchangeTo}
-                onChange={(e) => dispatch(setExchangeTo(e.target.value))} />
+        <div id="exchanger-container">
+            <h1 id="title">Currency converter</h1>
+            <h2 id="today">Today, {moment(new Date).format("MMM DD YYYY")}</h2>
+            <div id="convert-div">
+                <input
+                    id="input-sentence"
+                    placeholder="15 usd in rub"
+                    onKeyDown={pressEnter}
+                    value={input}
+                    onChange={(e) => dispatch(setInput(e.target.value))} />
+                <button id="convert-btn" onClick={() => dispatch(convertAsync(input))}>Convert</button>
+            </div>
+            <div id="from-to-container">
+            <div id="from-div">
+                <select onChange={(e) => dispatch(setBaseAsync(e.target.value))}>
+                    {Object.keys(symbols).map(item => {
+                        const text = `${item} - ${symbols[item].description}`
+                        return item === base ?
+                            <option value={item} selected="selected">{text}</option>
+                            : <option value={item}>{text}</option>
+                    })}
+                </select>
+                <input
+                    className="input-amount"
+                    placeholder="0"
+                    type="number"
+                    pattern="[0-9]*"
+                    value={exchangeFrom}
+                    onChange={(e) => dispatch(setExchangeFrom(e.target.value))} />
+            </div>
+            <div id="to-div">
+                <select id="select-to" onChange={(e) => dispatch(setTarget(e.target.value))}>
+                    {Object.keys(symbols).map(item => {
+                        const text = `${item} - ${symbols[item].description}`
+                        return item === target ?
+                            <option value={item} selected="selected">{text}</option>
+                            : <option value={item}>{text}</option>
+                    })}
+                </select>
+
+                <input
+                    className="input-amount"
+                    placeholder="0"
+                    id="input-to"
+                    type="number"
+                    pattern="[0-9]*"
+                    value={exchangeTo}
+                    onChange={(e) => dispatch(setExchangeTo(e.target.value))} />
+            </div>
+            </div>
         </div>
     )
 }
