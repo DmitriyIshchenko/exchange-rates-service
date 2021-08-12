8b1af42c041d016a766035391c35616039b628db
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2021-08-12 09:14:14 +0300

    Make the layout more adaptive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Exchanger } from "./features/exchanger/Exchanger"
+import { Graph } from "./features/graph/Graph"
 
 function App() {
   return (
-    <div>
+    <div id="app-container">
       <Exchanger />
+      <Graph />
     </div>
   )
 }
