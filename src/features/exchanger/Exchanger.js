829c9e5513d78ba3199a5193a2143c02df496b27
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-01-25 16:21:53 +0300

    Remove inputs from store and handle them locally

--- src/features/exchanger/Exchanger.js
+++ src/features/exchanger/Exchanger.js
@@ -1,88 +1,84 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-    selectExchangeFrom,
-    selectExchangeTo,
-    setExchangeFrom,
-    setExchangeTo,
     fetchSymbolsAsync,
-    setBaseAsync,
-    setTarget,
-    setInput,
-    convertAsync
+    fetchRatesAsync,
+    setBase,
+    setTarget
 } from "./exchangerSlice"
-import moment from 'moment';
-
 
 export function Exchanger() {
-    const exchangeFrom = useSelector(selectExchangeFrom);
-    const exchangeTo = useSelector(selectExchangeTo);
-    const symbols = useSelector((state) => state.exchanger.symbols);
-    const statusSymbols = useSelector((state) => state.exchanger.statusSymbols);
-    const statusRates = useSelector((state) => state.exchanger.statusRates);
-    const base = useSelector((state) => state.exchanger.base);
-    const target = useSelector((state) => state.exchanger.target);
-    const input = useSelector((state) => state.exchanger.input);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (statusSymbols === "idle") dispatch(fetchSymbolsAsync());
-        if (statusRates === "idle") dispatch(setBaseAsync(base));
-    })
 
-    function pressEnter(e) {
-        if (e.keyCode === 13) dispatch(convertAsync(input))
+    const dispatch = useDispatch();
+
+    const [sentence, setSentence] = useState("");
+    const [fromAmount, setFromAmount] = useState("");
+    const [toAmount, setToAmount] = useState("");
+
+    const symbols = useSelector(state => state.exchanger.symbols);
+    const rates = useSelector(state => state.exchanger.rates);
+    const base = useSelector(state => state.exchanger.base);
+    const target = useSelector(state => state.exchanger.target);
+
+    useEffect(() => {
+        dispatch(fetchSymbolsAsync())
+    }, [dispatch])
+
+    useEffect(() => {
+        dispatch(fetchRatesAsync(base))
+    }, [base, dispatch])
+
+    useEffect(() => {
+        setToAmount(Math.trunc(+fromAmount * rates[target] * 1000) / 1000)
+    }, [target, rates])
+
+    const handleConvert = () => {
+        const data = sentence.split(" "); //15 usd in rub
+        let [
+            amount,
+            from,
+            , //in
+            to
+        ] = data;
+        if (amount && from && to) {
+            from = from.toUpperCase();
+            to = to.toUpperCase();
+            if (!isNaN(+amount) && symbols.hasOwnProperty(from) && symbols.hasOwnProperty(to)) {
+                dispatch(setBase(from));
+                dispatch(setTarget(to));
+                setFromAmount(+amount);
+            }
+        } else return;
     }
+
+    const handleChangeFrom = (e) => {
+        setFromAmount(e.target.value);
+        setToAmount(Math.trunc(e.target.value * rates[target] * 1000) / 1000)
+    }
+    const handleChangeTo = (e) => {
+        setToAmount(e.target.value);
+        setFromAmount(Math.trunc(e.target.value / rates[target] * 1000) / 1000)
+    }
+
+    const options = Object.values(symbols).map(item => <option value={item.code} key={item.code}>{`${item.code} - ${item.description}`}</option>);
+
     return (
         <div id="exchanger-container">
-            <h1 id="title">Currency converter</h1>
-            <h2 id="today">Today, {moment(new Date).format("MMM DD YYYY")}</h2>
-            <div id="convert-div">
-                <input
-                    id="input-sentence"
-                    placeholder="15 usd in rub"
-                    onKeyDown={pressEnter}
-                    value={input}
-                    onChange={(e) => dispatch(setInput(e.target.value))} />
-                <button id="convert-btn" onClick={() => dispatch(convertAsync(input))}>Convert</button>
+            <div className='convert-sentence'>
+                <input type="text" value={sentence} onChange={(e) => setSentence(e.target.value)} />
+                <button onClick={handleConvert}>Convert</button>
             </div>
-            <div id="from-to-container">
-            <div id="from-div">
-                <select onChange={(e) => dispatch(setBaseAsync(e.target.value))}>
-                    {Object.keys(symbols).map(item => {
-                        const text = `${item} - ${symbols[item].description}`
-                        return item === base ?
-                            <option value={item} selected="selected">{text}</option>
-                            : <option value={item}>{text}</option>
-                    })}
-                </select>
-                <input
-                    className="input-amount"
-                    placeholder="0"
-                    type="number"
-                    pattern="[0-9]*"
-                    value={exchangeFrom}
-                    onChange={(e) => dispatch(setExchangeFrom(e.target.value))} />
-            </div>
-            <div id="to-div">
-                <select id="select-to" onChange={(e) => dispatch(setTarget(e.target.value))}>
-                    {Object.keys(symbols).map(item => {
-                        const text = `${item} - ${symbols[item].description}`
-                        return item === target ?
-                            <option value={item} selected="selected">{text}</option>
-                            : <option value={item}>{text}</option>
-                    })}
-                </select>
 
-                <input
-                    className="input-amount"
-                    placeholder="0"
-                    id="input-to"
-                    type="number"
-                    pattern="[0-9]*"
-                    value={exchangeTo}
-                    onChange={(e) => dispatch(setExchangeTo(e.target.value))} />
-            </div>
-            </div>
+            <select name="from" id="from" value={base} onChange={(e) => dispatch(setBase(e.target.value))}>
+                {options}
+            </select>
+            <input type="text" value={fromAmount} onChange={handleChangeFrom} />
+
+            <select name="to" id="to" value={target} onChange={(e) => dispatch(setTarget(e.target.value))}>
+                {options}
+            </select>
+            <input type="text" value={toAmount || ""} onChange={handleChangeTo} />
+
         </div>
     )
 }
--- src/features/exchanger/exchangerSlice.js
+++ src/features/exchanger/exchangerSlice.js
@@ -1,20 +1,16 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchSymbols, fetchLatestRates, convert } from "./exhangerAPI";
 const initialState = {
-    input: "",
-    exchangeFrom: "",
-    exchangeTo: "",
     base: "USD",
-    target: "RUB",
+    target: "USD",
     statusSymbols: "idle",
     symbols: {},
     statusRates: "idle",
     rates: {}
 }
 
-
-export const setBaseAsync = createAsyncThunk(
-    "exchanger/setBase",
+export const fetchRatesAsync = createAsyncThunk(
+    "exchanger/fetchRates",
     async (base) => {
         const response = await fetchLatestRates(base);
         return response;
@@ -29,36 +25,15 @@ export const fetchSymbolsAsync = createAsyncThunk(
     }
 )
 
-export const convertAsync = createAsyncThunk(
-    "exchanger/convert",
-    async (str) => {
-        const sentence = str.split(" ");
-        const amount = sentence[0];
-        const from = sentence[1].toUpperCase();
-        const to = sentence[3].toUpperCase();
-        const response = await convert(from, to, amount);
-        return response;
-    }
-)
-
 export const exchangerSlice = createSlice({
     name: "exchanger",
     initialState,
     reducers: {
-        setExchangeFrom: (state, action) => {
-            state.exchangeFrom = action.payload;
-            state.exchangeTo = (Math.trunc(+action.payload * state.rates[state.target] * 1000) / 1000) || "";
-        },
-        setExchangeTo: (state, action) => {
-            state.exchangeFrom = (Math.trunc(+action.payload / state.rates[state.target] * 1000) / 1000) || "";
-            state.exchangeTo = action.payload;
-        },
         setTarget: (state, action) => {
             state.target = action.payload;
-            state.exchangeTo = (Math.trunc(state.exchangeFrom * state.rates[state.target] * 1000) / 1000) || "";
         },
-        setInput: (state, action) => {
-            state.input = action.payload;
+        setBase: (state, action) => {
+            state.base = action.payload;
         }
     },
     extraReducers: (builder) => {
@@ -66,23 +41,13 @@ export const exchangerSlice = createSlice({
             state.statusSymbols = "succeeded";
             state.symbols = action.payload;
         });
-        builder.addCase(setBaseAsync.fulfilled, (state, action) => {
+        builder.addCase(fetchRatesAsync.fulfilled, (state, action) => {
             state.statusRates = "succeeded";
             state.rates = action.payload.rates;
-            state.base = action.payload.base;
-            state.exchangeTo = (Math.trunc(state.exchangeFrom * state.rates[state.target] * 1000) / 1000) || "";
-        })
-        builder.addCase(convertAsync.fulfilled, (state, action) => {
-            state.base = action.payload.query.from;
-            state.target = action.payload.query.to;
-            state.exchangeFrom = action.payload.query.amount;
-            state.exchangeTo = action.payload.result;
         })
     }
 })
 
+export const { setBase, setTarget } = exchangerSlice.actions;
 
-export const selectExchangeFrom = (state) => state.exchanger.exchangeFrom;
-export const selectExchangeTo = (state) => state.exchanger.exchangeTo;
-export const { setExchangeFrom, setExchangeTo, setTarget, setInput } = exchangerSlice.actions;
 export default exchangerSlice.reducer;
