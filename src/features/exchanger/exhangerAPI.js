120fe7bdeac753b7b7b4582764e35ad393889452
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-01-25 16:23:44 +0300

    Remove an unused request

--- src/features/exchanger/exhangerAPI.js
+++ src/features/exchanger/exhangerAPI.js
@@ -1,25 +1,19 @@
 
-export function fetchSymbols(){
+export function fetchSymbols() {
     return fetch('https://api.exchangerate.host/symbols')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        return data.symbols;
-    });
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data.symbols;
+        });
 }
-export function fetchLatestRates(base){
+export function fetchLatestRates(base) {
     return fetch(`https://api.exchangerate.host/latest?base=${base}`)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        return data;
-    }); 
-}
-
-export function convert(from,to,amount){
-    return fetch(`https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`)
-    .then((response)=>response.json())
-    .then(data=>data);
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        });
 }
